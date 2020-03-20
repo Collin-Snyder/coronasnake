895d04f7e3567b7349e0a9ae22b76329280f6423
Collin Snyder <collinjacobsnyder@gmail.com>
2020-03-20 14:16:14 -0500

    initial socket.io test before branching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./styles/main.css";
 import Board from "./components/Board.jsx";
+import io from "socket.io-client";
 
+const socket = io("localhost:4000");
+
+socket.on("test response", msg =>
+  document.getElementById("app").append(msg)
+);
 
 const App = () => {
-
   return (
-    <div className="app">
+    <div id="app">
       <h1>Snake</h1>
-      <Board/>
+      <Board />
+      <button
+        onClick={() => {
+          socket.emit("test", "Greetings, I come from the client.");
+        }}
+      >
+        Socket Test!
+      </button>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
@@ -10,9 +11,20 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to your Express-React app!");
-})
+  res.send(path.join(__dirname + "/../dist/index.html"));
+});
 
-app.listen(port, () => {
+var http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("test", msg => {
+    console.log(msg);
+    io.emit("test response", "Well hello there client!")
+  });
+});
+
+http.listen(port, () => {
   console.log(`Express yayyyyy server listening on port ${port}!`);
 });
